ff2b4c3ddce738c7fcd2fa2a52df6c337de9b69a
Plarget <67307148+Plarget@users.noreply.github.com>
2025-09-08 14:30:46 +0400

    a

--- src/shared/ui/IconButton/ui.tsx
+++ src/shared/ui/IconButton/ui.tsx
@@ -11,6 +11,7 @@ const IconButton: FC<TIconButton> = (props) => {
     ...rest
   } = props
 
+  console.log(children)
   return (
     <button
       className={classNames(className, "icon-button")}
